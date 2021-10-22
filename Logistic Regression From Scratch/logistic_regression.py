d2ac48922f06b695a4e7ca649fc21d3c4a285186
Anthony Nwachukwu <nwachukwuanthony2015@gmail.com>
2021-10-22 09:54:12 +0200

    Add empty methods for logistic regression class

---
+++ Logistic Regression From Scratch/logistic_regression.py
@@ -0,0 +1,43 @@
+#import libraries
+
+class LogisticRegression:
+
+    train_X = None
+    y_train = None
+
+    def decision_function(X):
+        """Predict confidence scores for samples"""
+        pass
+
+    def densify():
+        """Convert coefficient matrix to dense array format"""
+        pass
+
+    def fit(X, y[, sample_weight]):
+        """Fit the model according to the given training data"""
+        pass
+
+    def get_params([deep]):
+        """Get parameters for this estimator"""
+        pass
+
+    def predict(X):
+        """Predict class labels for samples in X"""
+        pass
+
+    def predict_log_proba(X):
+        """Predict logarithm of probability estimates"""
+        pass
+
+    def predict_proba(X):
+        """Probability estimates"""
+        pass
+
+    def score(X, y[, sample_weight]):
+        """Return the mean accuracy on the given test data and labels"""
+        pass
+
+    def set_params(**params):
+        """Set the parameters of this estimator"""
+        pass
+
